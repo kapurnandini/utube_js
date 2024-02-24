e3db3174adec78b7a88cabb443e747803d97f0d3
kapurnandini <131610571+kapurnandini@users.noreply.github.com>
2024-02-24 06:49:00 +0000

    conversion of datatypes

---
+++ 01_basics/conversion_03.js
@@ -0,0 +1,39 @@
+//1.converting to number
+let score = "hitesh"
+
+//Ways to display datatype
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+//Converts string/null/boolean/undefined to number
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+//null =>0\
+//undefined =>NaN(Not a Number)
+
+
+//2.converting to boolean
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+//3.converting to string
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+//converts into string o/p as 33
