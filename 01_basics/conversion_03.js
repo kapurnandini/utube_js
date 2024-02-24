0ad46e7df9f9000c15677457df62c1fdd400f0f7
kapurnandini <131610571+kapurnandini@users.noreply.github.com>
2024-02-24 13:26:22 +0000

    operators

--- 01_basics/conversion_03.js
+++ 01_basics/conversion_03.js
@@ -37,3 +37,48 @@ let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
 //converts into string o/p as 33
+
+//*********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);=> power
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); =>o/p = 12
+// console.log(1 + "2"); =>o/p = 12
+// console.log("1" + 2 + 2); =>o/p = 122
+// console.log(1 + 2 + "2"); =>o/p = 32
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+//not readable
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+//postfix and prefix
+let gameCounter = 100
+y = ++gameCounter;
+z= gameCounter++;
+console.log(gameCounter);
+console.log(z)
+console.log(y)
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
