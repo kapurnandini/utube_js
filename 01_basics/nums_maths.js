e84632754bb28bc4ab3d7ed52679f149da8145f7
kapurnandini <131610571+kapurnandini@users.noreply.github.com>
2024-02-25 13:27:24 +0000

    maths and dates

---
+++ 01_basics/nums_maths.js
@@ -0,0 +1,23 @@
+const score = 400
+// console.log(score);
+//returns a number value
+
+const balance = new Number(100)
+ console.log(balance);
+//returns number as object
+
+// console.log(balance.toString().length);
+//converts numbers to string and tells length of the string
+
+console.log(balance.toFixed(1));
+//converts number to decimal till 1
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4));
+//converts to precised value
+
+const hundreds = 1000000
+ console.log(hundreds.toLocaleString('en-IN'));
+//en-in converts to indian number system
+
