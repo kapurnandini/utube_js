2b9211c081259e66233d3a771a80280f519da6d3
kapurnandini <131610571+kapurnandini@users.noreply.github.com>
2024-03-05 16:21:13 +0000

    date and time

--- 01_basics/date_time.js
+++ 01_basics/date_time.js
@@ -8,16 +8,19 @@ console.log(datee.toLocaleTimeString())
 // console.log(typeof myDate);=>object
 
 // let myCreatedDate = new Date(2023, 0, 23) => to create ur own date
+
+//month starts from 0 in javascript
+
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3) => 5 &3 for hours &minutes(month starts from 0)
 // let myCreatedDate = new Date("2023-01-14") => here month starts from 1
 let myCreatedDate = new Date("01-14-2023")
 // console.log(myCreatedDate.toLocaleString());
 
-let myTimeStamp = Date.now()
+let myTimeStamp = Date.now()//gives date in milliseconds
 
 // console.log(myTimeStamp);
-// console.log(myCreatedDate.getTime());
-// console.log(Math.floor(Date.now()/1000));
+// console.log(myCreatedDate.getTime());//gives time from a particular date
+// console.log(Math.floor(Date.now()/1000)); //converts to integer and converts to sec
 
 let newDate = new Date()
 console.log(newDate);
