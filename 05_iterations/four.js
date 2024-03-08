173474a82f1f07771080004537ee32890eed0221
kapurnandini <131610571+kapurnandini@users.noreply.github.com>
2024-03-08 18:17:47 +0000

    for in

---
+++ 05_iterations/four.js
@@ -0,0 +1,29 @@
+// for of loop on printing gives values of both keys and values 
+// for in loop on printing gives values of only keys , we can access values using obj[keys]
+// const myObject = {
+//     js: 'javascript',
+//     cpp: 'C++',
+//     rb: "ruby",
+//     swift: "swift by apple"
+// }
+
+// for (const key in myObject) {
+//     //console.log(`${key} shortcut is for ${myObject[key]}`);
+// }
+
+// const programming = ["js", "rb", "py", "java", "cpp"]
+
+// for (const key in programming) {
+//     //console.log(programming[key]);
+// }
+
+//for below eg we cant use for in for map
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+for (const key in map) {
+    console.log(key);
+}
