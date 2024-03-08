7fba94de8616782db0e77ae64f51c4f1662cbe79
kapurnandini <131610571+kapurnandini@users.noreply.github.com>
2024-03-08 18:10:37 +0000

    for of

---
+++ 05_iterations/three.js
@@ -0,0 +1,40 @@
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    //console.log(num);
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    //console.log(`Each char is ${greet}`)
+}
+
+// Maps=> holds only unique values
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+
+// console.log(map);
+//prints distinguish values for keys and values
+for (const [key, value] of map) {
+    // console.log(key, ':-', value);
+}
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+//we cant iterate objects using for of loop
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+    
+// }
